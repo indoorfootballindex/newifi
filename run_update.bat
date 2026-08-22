9a46df2636c278ec3b41f88b0b6be29e56151838
indoorfootballindex <indoorfootballindex@gmail.com>
2026-08-22 11:35:44 -0500

    update

---
+++ run_update.bat
@@ -0,0 +1,8 @@
+@echo off
+py update_site.py %*
+echo.
+echo ================================================
+echo Done. Scroll up to see everything that happened.
+echo Press any key to close this window...
+echo ================================================
+pause >nul
